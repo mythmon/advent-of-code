cfbcc67ba5370fd7e069459939f5692f81ea3d91
Mike Cooper <mythmon@gmail.com>
2017-12-04 21:34:19 -0800

    day 5

---
+++ src/day5/part1.rs
@@ -0,0 +1,31 @@
+#![feature(range_contains)]
+
+fn main() {
+    let input: &'static str = include_str!("input.txt");
+    let input: Vec<i32> = input.lines().map(|l| l.parse().unwrap()).collect();
+    println!("{}", puzzle(input));
+}
+
+fn puzzle(mut input: Vec<i32>) -> u32 {
+    let mut steps: u32 = 0;
+    let mut pc: i32 = 0;
+
+    let bounds = 0..(input.len() as i32);
+
+    while bounds.contains(pc) {
+        steps += 1;
+        let next = pc + input[pc as usize];
+        if !bounds.contains(next) {
+            break;
+        }
+        input[pc as usize] += 1;
+        pc = next;
+    }
+
+    steps
+}
+
+#[test]
+fn test_example() {
+    assert_eq!(puzzle(vec![0, 3, 0, 1, -3]), 5);
+}
