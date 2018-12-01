60998060ce059061e35a183ffaf3f7ae71688670
Mike Cooper <mythmon@gmail.com>
2018-11-30 21:55:05 -0500

    subcommands and filtering

--- src/bin.rs
+++ src/bin.rs
@@ -1,64 +1,95 @@
 #![deny(clippy::all)]
 
+use clap::{crate_version, App, AppSettings, Arg, SubCommand};
 use colored::Colorize;
 use rayon::prelude::*;
 use std::collections::BTreeMap;
 
 use advent::{
     cases::Puzzle,
-    day01::part1::Day01Part1,
-    day01::part2::Day01Part2,
-    day02::part1::Day02Part1,
-    day02::part2::Day02Part2,
-    day03::part1::Day03Part1,
-    day03::part2::Day03Part2,
-    day04::part1::Day04Part1,
-    day04::part2::Day04Part2,
-    day05::part1::Day05Part1,
-    day05::part2::Day05Part2,
-    day06::part1::Day06Part1,
-    day06::part2::Day06Part2,
-    day07::part1::Day07Part1,
-    day07::part2::Day07Part2,
-    day08::part1::Day08Part1,
-    day08::part2::Day08Part2,
-    day09::part1::Day09Part1,
-    day09::part2::Day09Part2,
-    day10::part1::Day10Part1,
-    day10::part2::Day10Part2,
-    day11::part1::Day11Part1,
-    day11::part2::Day11Part2,
-    day12::part1::Day12Part1,
-    day12::part2::Day12Part2,
-    day13::part1::Day13Part1,
-    day13::part2::Day13Part2,
-    day14::part1::Day14Part1,
-    day14::part2::Day14Part2,
-    day15::part1::Day15Part1,
-    day15::part2::Day15Part2,
-    day16::part1::Day16Part1,
-    day16::part2::Day16Part2,
-    day17::part1::Day17Part1,
-    day17::part2::Day17Part2,
-    day18::part1::Day18Part1,
-    day18::part2::Day18Part2,
-    day19::part1::Day19Part1,
-    day19::part2::Day19Part2,
-    day20::part1::Day20Part1,
-    day20::part2::Day20Part2,
-    day21::part1::Day21Part1,
-    day21::part2::Day21Part2,
-    day22::part1::Day22Part1,
-    day22::part2::Day22Part2,
-    day23::part1::Day23Part1,
-    day23::part2::Day23Part2,
-    day24::part1::Day24Part1,
-    day24::part2::Day24Part2,
+    day01::{part1::Day01Part1, part2::Day01Part2},
+    day02::{part1::Day02Part1, part2::Day02Part2},
+    day03::{part1::Day03Part1, part2::Day03Part2},
+    day04::{part1::Day04Part1, part2::Day04Part2},
+    day05::{part1::Day05Part1, part2::Day05Part2},
+    day06::{part1::Day06Part1, part2::Day06Part2},
+    day07::{part1::Day07Part1, part2::Day07Part2},
+    day08::{part1::Day08Part1, part2::Day08Part2},
+    day09::{part1::Day09Part1, part2::Day09Part2},
+    day10::{part1::Day10Part1, part2::Day10Part2},
+    day11::{part1::Day11Part1, part2::Day11Part2},
+    day12::{part1::Day12Part1, part2::Day12Part2},
+    day13::{part1::Day13Part1, part2::Day13Part2},
+    day14::{part1::Day14Part1, part2::Day14Part2},
+    day15::{part1::Day15Part1, part2::Day15Part2},
+    day16::{part1::Day16Part1, part2::Day16Part2},
+    day17::{part1::Day17Part1, part2::Day17Part2},
+    day18::{part1::Day18Part1, part2::Day18Part2},
+    day19::{part1::Day19Part1, part2::Day19Part2},
+    day20::{part1::Day20Part1, part2::Day20Part2},
+    day21::{part1::Day21Part1, part2::Day21Part2},
+    day22::{part1::Day22Part1, part2::Day22Part2},
+    day23::{part1::Day23Part1, part2::Day23Part2},
+    day24::{part1::Day24Part1, part2::Day24Part2},
     day25::Day25,
 };
 
 fn main() {
-    let parts: Vec<Box<dyn Puzzle>> = vec![
+    let matches = App::new("Advent")
+        .version(crate_version!())
+        .settings(&[
+            AppSettings::ColoredHelp,
+            AppSettings::ArgsNegateSubcommands,
+            AppSettings::DeriveDisplayOrder,
+            AppSettings::InferSubcommands,
+            AppSettings::SubcommandRequiredElseHelp,
+            AppSettings::UnifiedHelpMessage,
+        ])
+        .arg(
+            Arg::with_name("verbose")
+                .global(true)
+                .multiple(true)
+                .short("v")
+                .long("verbose")
+                .help("Print more verbose output"),
+        )
+        .subcommand(
+            SubCommand::with_name("run")
+                .about("Runs puzzles")
+                .setting(AppSettings::ColoredHelp)
+                .arg(
+                    Arg::with_name("filter")
+                        .takes_value(true)
+                        .short("f")
+                        .long("filter")
+                        .help("Only run tests whose name contains this string")
+                        .required(false),
+                ),
+        )
+        .get_matches();
+
+    match matches.subcommand() {
+        ("run", Some(opts)) => run(opts),
+        _ => unreachable!(),
+    }
+}
+
+struct RunOptions {
+    filter: Option<String>,
+    verbose: bool,
+}
+
+impl<'a> From<&clap::ArgMatches<'a>> for RunOptions {
+    fn from(matches: &clap::ArgMatches) -> Self {
+        Self {
+            filter: matches.value_of("filter").map(|f| f.to_owned()),
+            verbose: matches.is_present("verbose"),
+        }
+    }
+}
+
+fn get_puzzles() -> Vec<Box<dyn Puzzle>> {
+    vec![
         Box::new(Day01Part1 {}),
         Box::new(Day01Part2 {}),
         Box::new(Day02Part1 {}),
@@ -108,9 +139,25 @@ fn main() {
         Box::new(Day24Part1 {}),
         Box::new(Day24Part2 {}),
         Box::new(Day25 {}),
-    ];
+    ]
+}
 
-    parts
+fn run<O>(opts: O)
+where
+    O: Into<RunOptions>,
+{
+    let opts = opts.into();
+    let mut puzzles = get_puzzles();
+
+    if let Some(ref filter) = opts.filter {
+        let filter = filter.to_lowercase();
+        puzzles = puzzles
+            .into_iter()
+            .filter(|p| p.name().to_lowercase().contains(&filter))
+            .collect();
+    }
+
+    puzzles
         .par_iter()
         .flat_map(|part| {
             part.cases()
@@ -126,23 +173,35 @@ fn main() {
         .iter()
         .fold(BTreeMap::new(), |mut map, (part, case, result)| {
             map.entry(part.name())
-                .or_insert(vec![])
+                .or_insert_with(Vec::new)
                 .push((case, result));
             map
         })
         .iter()
         .for_each(|(group_name, results)| {
             print!("{} ", group_name);
-            for (_, result) in results {
-                if let Ok(_) = result {
-                    print!("{}", "✔".green());
-                } else {
-                    print!("{}", "✗".red());
+            if opts.verbose {
+                println!();
+                for (case, result) in results {
+                    if result.is_ok() {
+                        print!("    {} ", "PASS".green());
+                    } else {
+                        print!("    {} ", "FAIL".red());
+                    }
+                    println!("{}", case.name());
                 }
-            }
-            println!();
-            for (case, _) in results.iter().filter(|(_, result)| result.is_err()) {
-                println!("  fail {} - expected <?> got <?>", case.name());
+            } else {
+                for (_, result) in results {
+                    if result.is_ok() {
+                        print!("{}", "✔".green());
+                    } else {
+                        print!("{}", "✗".red());
+                    }
+                }
+                println!();
+                for (case, _) in results.iter().filter(|(_, result)| result.is_err()) {
+                    println!("  FAIL {}", case.name());
+                }
             }
         });
 }
